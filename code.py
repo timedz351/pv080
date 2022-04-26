78ad1fe580bbd0db0d2cab1fbfd819569bbc0a67
timke <swisiak333@gmail.com>
2022-04-26 16:59:13 +0200

    fix2

--- code.py
+++ code.py
@@ -6,13 +6,13 @@ import flask
 
 
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 
 def load_config(filename):
     # Load a configuration file into YAML
-    stream = file.open(filename, "w")
+    stream = filename.open(filename, "w")
     config = yaml.load(stream)
 
 
@@ -34,6 +34,7 @@ def fetch_website(urllib_version, url):
 
 @app.route("/")
 def index():
+    # Insallah
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
