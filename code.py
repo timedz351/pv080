24cb74c8751162b150b0432cb9951a6ac7685d1e
timke <swisiak333@gmail.com>
2022-04-26 16:51:33 +0200

    fix

--- code.py
+++ code.py
@@ -27,8 +27,8 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
-    return r.data
+    request = http.request('GET', url)
+    return request.data
 
 
 
